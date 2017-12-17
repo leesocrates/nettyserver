b2f8f0807c66e3f558d772dd9d5041696d24309c
socrate <leesocrate@gmail.com>
2017-12-17 18:21:33 +0800

    use new version getUserMedia interface

--- src/main/resources/js/wetrtcClient.js
+++ src/main/resources/js/wetrtcClient.js
@@ -1,22 +1,20 @@
 //checks if the browser supports WebRTC 
 
 function hasUserMedia() { 
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia; 
+   navigator.getUserMedia =  navigator.mediaDevices.getUserMedia
    return !!navigator.getUserMedia; 
 }
  
 if (hasUserMedia()) { 
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-		
-   //get both video and audio streams from user's camera 
-   navigator.getUserMedia({ video: true, audio: false }, function (stream) { 
+   var handleStream = function (stream) { 
       var video = document.querySelector('video'); 
 		
       //insert stream into the video tag 
       video.src = window.URL.createObjectURL(stream); 
-   }, function (err) {}); 
+   }
+		
+   //get both video and audio streams from user's camera 
+   navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(handleStream); 
 }else {
    alert("Error. WebRTC is not supported!"); 
 }
