c2f84c9cce395d4fa6c986192509380bbf4eebd9
lixiangxiang <xiangxiang@eastmoney.com>
2017-12-26 11:17:18 +0800

    annotate the wss code

--- src/main/resources/js/webrtc_server.js
+++ src/main/resources/js/webrtc_server.js
@@ -1,39 +1,39 @@
-var https=require('https');
-var ws=require('ws');
-var fs=require('fs');
-var keypath=process.cwd()+'/server.key';//我把秘钥文件放在运行命令的目录下测试
-var certpath=process.cwd()+'/server.crt';//console.log(keypath);
-//console.log(certpath);
- 
-var options = {
- key: fs.readFileSync(keypath),
- cert: fs.readFileSync(certpath),
-
-};
- 
-var server=https.createServer(options, function (req, res) {//要是单纯的https连接的话就会返回这个东西
- res.writeHead(403);//403即可
- res.end("This is a WebSockets server!");
-}).listen(9090);
- 
- 
-var wss = new ws.Server( { server: server } );//把创建好的https服务器丢进websocket的创建函数里，ws会用这个服务器来创建wss服务
-//同样，如果丢进去的是个http服务的话那么创建出来的还是无加密的ws服务
-//wss.on( 'connection', function ( wsConnect ) {
-// wsConnect.on( 'message', function ( message ) {
-//  console.log( message );
-// });
-//});
-
-
-
-
-////require our websocket library 
-//var WebSocketServer = require('ws').Server; 
-//
-////creating a websocket server at port 9090 
-//var wss = new WebSocketServer({port: 9090}); 
+//var https=require('https');
+//var ws=require('ws');
+//var fs=require('fs');
+//var keypath=process.cwd()+'/server.key';//我把秘钥文件放在运行命令的目录下测试
+//var certpath=process.cwd()+'/server.crt';//console.log(keypath);
+////console.log(certpath);
 // 
+//var options = {
+// key: fs.readFileSync(keypath),
+// cert: fs.readFileSync(certpath),
+//
+//};
+// 
+//var server=https.createServer(options, function (req, res) {//要是单纯的https连接的话就会返回这个东西
+// res.writeHead(403);//403即可
+// res.end("This is a WebSockets server!");
+//}).listen(9090);
+// 
+// 
+//var wss = new ws.Server( { server: server } );//把创建好的https服务器丢进websocket的创建函数里，ws会用这个服务器来创建wss服务
+////同样，如果丢进去的是个http服务的话那么创建出来的还是无加密的ws服务
+////wss.on( 'connection', function ( wsConnect ) {
+//// wsConnect.on( 'message', function ( message ) {
+////  console.log( message );
+//// });
+////});
+
+
+
+
+//require our websocket library 
+var WebSocketServer = require('ws').Server; 
+
+//creating a websocket server at port 9090 
+var wss = new WebSocketServer({port: 9090}); 
+ 
 //all connected to the server users
 var users = {};
 
