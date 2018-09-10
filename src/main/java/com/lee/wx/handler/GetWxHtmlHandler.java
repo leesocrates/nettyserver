160df7083240c2e42a20850c57f5356c7610f757
leesocrates <leesocrate@gmail.com>
2018-09-10 15:10:51 +0800

    微信网页端获取accesstoken

--- src/main/java/com/lee/wx/handler/GetWxHtmlHandler.java
+++ src/main/java/com/lee/wx/handler/GetWxHtmlHandler.java
@@ -1,16 +1,26 @@
 package com.lee.wx.handler;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.lee.retrofit.handler.GetHtmlHandler;
 import com.lee.utils.Constants;
 import com.lee.utils.FileUtils;
 import com.lee.utils.HttpUtils;
+import com.lee.wx.entity.AccessToken;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.router.Routed;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
@@ -48,9 +58,17 @@ public class GetWxHtmlHandler extends
             String queryStrs = uriPath.substring(uriPath.indexOf("?")+1);
             String[] splitstrs = queryStrs.split("&");
             System.out.println("splitStrs is : "+splitstrs);
-
+            String[] codes = splitstrs[0].split("=");
+            if(codes.length==2){
+                String code = codes[1];
+                try {
+                    sendFetchTokenRequest(code);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        String fileName = "test.html";
+        String fileName = "test1.html";
         System.out.println("fileName is : " + fileName);
         try {
             InputStream in = GetHtmlHandler.class.getClassLoader()
@@ -71,6 +89,39 @@ public class GetWxHtmlHandler extends
             e.printStackTrace();
         }
 
+    }
+
+    public void sendFetchTokenRequest(String code) throws Exception {
+
+        URL serverUrl = new URL("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxdfe88205b45a1aaa&secret=d58d13c8be552716c7d28811e77a7166&code="+code+"&grant_type=authorization_code");
+        HttpURLConnection conn = (HttpURLConnection) serverUrl.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        //必须设置false，否则会自动redirect到重定向后的地址
+        conn.setInstanceFollowRedirects(false);
+        conn.connect();
+        String result = getReturn(conn);
+        Gson gson = new Gson();
+        Type type = new TypeToken<AccessToken>() {}.getType();
+        AccessToken accessToken = gson.fromJson(result, type);
+
+        System.out.println("getWxHtmlHandler result is : "+result);
+    }
+
+    /*请求url获取返回的内容*/
+    private String getReturn(HttpURLConnection connection) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        //将返回的输入流转换成字符串
+        try (InputStream inputStream = connection.getInputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("utf-8"));
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);) {
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+            String result = buffer.toString();
+            return result;
+        }
     }
 
 }
